632b1b44be3ab01fe4be2af0e31cabc1e33b83e7
Mike <MikeFvv@gmail.com>
2017-12-22 02:46:19 +0800

    上传

--- MMBTCVest/MMBTCVest/MMConfig/AppDelegate+ThirdService.m
+++ MMBTCVest/MMBTCVest/MMConfig/AppDelegate+ThirdService.m
@@ -58,7 +58,7 @@
     // init Push
     // notice: 2.1.5版本的SDK新增的注册方法，改成可上报IDFA，如果没有使用IDFA直接传nil
     // 如需继续使用pushConfig.plist文件声明appKey等配置内容，请依旧使用[JPUSHService setupWithOption:launchOptions]方式初始化。
-    [JPUSHService setupWithOption:self.launchOptions appKey:@"e438667966a7770fb43c9fb2"
+    [JPUSHService setupWithOption:self.launchOptions appKey:@"4ed614100e20c11201aabc09"
                           channel:nil
                  apsForProduction:FALSE
             advertisingIdentifier:advertisingId];
